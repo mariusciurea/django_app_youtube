8671c960816df7d70ae1b672553364098a3a9766
Marius Ciurea <ciurea.marius1@gmail.com>
2022-04-13 09:47:06 +0300

    'delete_data'

--- lista_cumparaturi/produse/urls.py
+++ lista_cumparaturi/produse/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('delete/<id>', views.delete, name='delete'),
+    path('cumpara/<id>', views.change_status, name='change_status'),
 ]
